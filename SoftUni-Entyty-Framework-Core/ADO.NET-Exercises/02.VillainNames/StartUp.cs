4f5c8f766a70a6c029f587d29a7439ce56875485
ItsAlphaHelix <mitak4000@gmail.com>
2022-06-24 18:12:41 +0300

    SoftUni-ADO.NET problem solutions.

---
+++ SoftUni-Entyty-Framework-Core/ADO.NET-Exercises/02.VillainNames/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace VillainNames
+{
+    using Microsoft.Data.SqlClient;
+    using System;
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection(InitialSetup.Config.ConnectionString);
+
+            connection.Open();
+
+            string query = @"SELECT Name, COUNT(mv.MinionId)
+                               FROM Villains AS v
+                               JOIN MinionsVillains AS mv ON mv.VillainId = v.Id
+                               GROUP BY v.Id, v.Name
+                               HAVING COUNT(mv.MinionId) > 3";
+
+            using SqlCommand commnad = new SqlCommand(query, connection);
+
+            using SqlDataReader reader = commnad.ExecuteReader();
+
+            while (reader.Read())
+            {
+                var name = reader[0];
+                var count = reader[1];
+
+                Console.WriteLine($"{name} - {count}");
+            }
+        }
+    }
+}
