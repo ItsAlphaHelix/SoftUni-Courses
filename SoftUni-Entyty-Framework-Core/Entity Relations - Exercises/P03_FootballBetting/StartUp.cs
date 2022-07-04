e696e644e7adbb93a9abc7b2fac3c3dc79546171
ItsAlphaHelix <mitak4000@gmail.com>
2022-07-04 12:48:29 +0300

    Entity Relations - problem solutions.

---
+++ SoftUni-Entyty-Framework-Core/Entity Relations - Exercises/P03_FootballBetting/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace P03_FootballBetting
+{
+    using P03_FootballBetting.Data;
+    using System;
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var db = new FootballBettingContext();
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+        }
+    }
+}
