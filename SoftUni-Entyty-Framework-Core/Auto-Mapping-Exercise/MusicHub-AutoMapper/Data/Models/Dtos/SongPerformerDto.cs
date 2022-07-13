d6c65a2f5074e3805339af9dc60e977c9b5f6f39
ItsAlphaHelix <mitak4000@gmail.com>
2022-07-13 16:28:55 +0300

    Solving with auto mapper an old task

---
+++ SoftUni-Entyty-Framework-Core/Auto-Mapping-Exercise/MusicHub-AutoMapper/Data/Models/Dtos/SongPerformerDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MusicHub.Data.Models.Dtos
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class SongPerformerDto
+    {
+        public string PerformerFirstName { get; set; }
+        public string PerformerLastName { get; set; }
+    }
+}
