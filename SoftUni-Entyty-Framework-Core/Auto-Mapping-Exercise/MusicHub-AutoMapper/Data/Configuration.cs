8b3e519f882ddbc8ae02434e8a5a4deeb5517f49
ItsAlphaHelix <mitak4000@gmail.com>
2022-07-13 16:33:18 +0300

    Solving with auto mapper an old task.

--- SoftUni-Entyty-Framework-Core/Auto-Mapping-Exercise/MusicHub-AutoMapper/Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MusicHub.Data
-{
-   public static class Configuration
-    {
-        public static string ConnectionString =
-            @"Server=.;Database=SoftUni;User Id=sa;Password=my!Password;TrustServerCertificate=True";
-    }
-}
