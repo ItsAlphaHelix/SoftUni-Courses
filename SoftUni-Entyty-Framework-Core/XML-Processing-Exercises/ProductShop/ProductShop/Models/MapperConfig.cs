e3a896d95afa5d505b8868d2864c239aad9620ac
ItsAlphaHelix <mitak4000@gmail.com>
2022-07-20 14:13:45 +0300

    XML-Processing - problem solutions.

---
+++ SoftUni-Entyty-Framework-Core/XML-Processing-Exercises/ProductShop/ProductShop/Models/MapperConfig.cs
@@ -0,0 +1,13 @@
+﻿
+namespace ProductShop.Models
+{
+    using AutoMapper;
+    using System;
+    public class MapperConfig
+    {
+        public static MapperConfiguration config = new MapperConfiguration(config =>
+        {
+            config.AddProfile<ProductShopProfile>();
+        });
+    }
+}
