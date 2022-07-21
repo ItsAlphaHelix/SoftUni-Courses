0e92c9ffc4b40cd9ff161e2e04821331fea9397d
ItsAlphaHelix <mitak4000@gmail.com>
2022-07-21 12:51:26 +0300

    XML-Processing - problem solutions.

---
+++ SoftUni-Entyty-Framework-Core/XML-Processing-Exercises/ProductShop/ProductShop/Dtos/Export/UserProductDto.cs
@@ -0,0 +1,18 @@
+﻿namespace ProductShop.Dtos.Export
+{
+    using System;
+    using System.Xml.Serialization;
+
+    [XmlType("User")]
+    public class UserProductDto
+    {
+        [XmlElement("firstName")]
+        public string FirstName { get; set; }
+
+        [XmlElement("lastName")]
+        public string LastName { get; set; }
+
+        [XmlElement("soldProducts")]
+        public SoldProductDto[] SoldProducts { get; set; }
+    }
+}
---
+++ SoftUni-Entyty-Framework-Core/XML-Processing-Exercises/ProductShop/ProductShop/Dtos/Import/ImportUserDto.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ProductShop.Dtos.Import
+{
+    using System;
+    using System.Xml.Serialization;
+
+    [XmlType("User")]
+    public class ImportUserDto
+    {
+        [XmlElement("firstName")]
+        public string FirstName { get; set; }
+
+        [XmlElement("LastName")]
+        public string LastName { get; set; }
+
+        [XmlElement("age")]
+        public int Age { get; set; }
+    }
+}
