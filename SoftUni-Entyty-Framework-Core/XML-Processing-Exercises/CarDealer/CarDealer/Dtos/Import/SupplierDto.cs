08e287ce5b0943e0f5b008ccbbaaea2634cfe077
ItsAlphaHelix <mitak4000@gmail.com>
2022-07-22 09:01:43 +0300

    XML-Processing - problem solutions.

---
+++ SoftUni-Entyty-Framework-Core/XML-Processing-Exercises/CarDealer/CarDealer/Dtos/Import/SupplierDto.cs
@@ -0,0 +1,16 @@
+﻿
+namespace CarDealer.Dto.Import
+{
+    using System;
+    using System.Xml.Serialization;
+
+    [XmlType("Supplier")]
+    public class SupplierDto
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("isImporter")]
+        public bool IsImporter { get; set; }
+    }
+}
---
+++ SoftUni-Entyty-Framework-Core/XML-Processing-Exercises/CarDealer/CarDealer/Models/MapperConfig.cs
@@ -0,0 +1,13 @@
+﻿
+namespace CarDealer.Models
+{
+    using AutoMapper;
+    using System;
+    public class MapperConfig
+    {
+        public static MapperConfiguration config = new MapperConfiguration(config =>
+        {
+            config.AddProfile<CarDealerProfile>();
+        });
+    }
+}
